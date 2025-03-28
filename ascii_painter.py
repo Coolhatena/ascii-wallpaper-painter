8936635d75f90f9d93b23ce157e42cc5d969cf47
Coolhatena <ingsc.geffroy@gmail.com>
2025-03-28 01:19:32 -0700

    Replace loops with numpy operations to make transformation almost instantly

--- ascii_painter.py
+++ ascii_painter.py
@@ -1,20 +1,12 @@
 import cv2
-import numpy as np
 
-img_name = 'image1.png'
+img_name = 'notext.png'
+img = cv2.imread(img_name, cv2.IMREAD_UNCHANGED)
 
-img = cv2.imread(img_name,  cv2.IMREAD_UNCHANGED)
-height, width, depth = img.shape
-img_res = img.copy()
-print(img[0][0])
+mask = img[:, :, 3] != 0
 
+img[mask] = [255, 255, 255, 255]
 
-for i, row in enumerate(img):
-	for j, px in enumerate(row):
-		_, _, _, alpha = px
-		if alpha != 0:
-			img_res[i][j] = np.array([255, 255, 255, 255])
-
-cv2.imwrite('result.png', img_res)
+cv2.imwrite(f'result_{img_name}', img)
 
 
